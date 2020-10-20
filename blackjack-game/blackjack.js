802985de529ba68f7739f24f122ec2497f8fd2c5
gravinblumenn <gavinblumenn@gmail.com>
2020-10-20 11:40:21 -0600

    starting blackjac game

---
+++ blackjack-game/blackjack.js
@@ -0,0 +1,24 @@
+const {getHandTotal, displayHand,} = require('./playerHand.js');
+const {createDeck} = require('./deck.js');
+const {createPlayer } = require('./player.js');
+const {dealHand } = require('./dealer.js');
+
+function startGame() {
+    console.log('\n\n');
+    console.log('BLACKJACK GAME');
+
+    const player = createPlayer('Gavin', 50.00);
+    const deck = createDeck();
+
+    console.log(`Created player ${player.name} with $${player.money}`);
+    console.log('Dealing hand...');
+
+    dealHand(deck, player.hand);
+
+    displayHand(player.hand)
+    let handTotal = getHandTotal(player.hand);
+
+    console.log(`Player hand total: ${handTotal}`);
+}
+
+startGame();
---
+++ blackjack-game/player.js
@@ -0,0 +1,11 @@
+function createPlayer(name, money = 100.00) {
+    return {
+        name,
+        money,
+        hand: [],
+    }
+    return player;
+}
+module.exports = {
+    createPlayer
+}
