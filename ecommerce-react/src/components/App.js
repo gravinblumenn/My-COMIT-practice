1eea4de701c5b281b1b04d9c8e5ae2859c6ed9e5
gravinblumenn <gavinblumenn@gmail.com>
2020-11-09 19:02:11 -0600

    comit after class

--- ecommerce-react/src/components/App.js
+++ ecommerce-react/src/components/App.js
@@ -1,11 +1,12 @@
 import React, { useState} from 'react'
+import {orderBy} from 'lodash'
 
 import Header from './Header';
 import ItemCategories from './ItemCategories';
 import ItemsList from './ItemsList';
 import Footer from './Footer';
 
-import { getItems } from '../data';
+import { getItems, getItemFields } from '../data';
 
 export default function App() {
     const [user, setUser] = useState({
@@ -13,24 +14,47 @@ export default function App() {
         cart: [],
 
     });
+
+    const [selectedCategory, setSelectedCategory] = useState('all');
+    const [selectedSortField, setSelectedSortField] = useState('id')
+
     const items = getItems();
+    const itemFields = getItemFields();
 
         /* looking at the value of isLoggedIn, based on the value, the function setIsLoggedIn, changes the variable IsLoggedIn */
         const handleLogInClick = () => {
             setUser({...user, isLoggedIn: !user.isLoggedIn}); //using spread operator because we cannot update the original DOM
-        }
+        };
 
         const handleAddToCartClick = (item) => {
             console.log(item);
             const updatedCart = [...user.cart, item]
             setUser({...user, cart: updatedCart});
-        }
+        };
+
+        const handleSelectCategory = (category) => {
+            console.log(category);
+            setSelectedCategory(category);
+        };
+
+        const handleSelectSortField = (field) => {
+            console.log(field);
+            setSelectedSortField(field);
+        };
+
+        const getItemsToDisplay = () => {
+            const filteredItems =  items.filter(
+                (item) => 
+                selectedCategory === "all" || item.category === selectedCategory);
+                return orderBy (filteredItems, selectedSortField, "asc");
+        };
+
 
     return (
         <div className="container">
             <Header isLoggedIn = {user.isLoggedIn} handleClick = {handleLogInClick} />
-            <ItemCategories />
-            <ItemsList items={items} handleAddToCartClick = {handleAddToCartClick} />
+            <ItemCategories handleSelectCategory = {handleSelectCategory}/>
+            <ItemsList items={getItemsToDisplay()} handleAddToCartClick = {handleAddToCartClick} />
             <Footer />
         </div>
     );
--- ecommerce-react/src/data.js
+++ ecommerce-react/src/data.js
@@ -1,34 +1,52 @@
 export function getItems() {
     return [
             {
+                id: 1,
                 name: 'Soccer ball',
                 price: 44.00,
                 description: 'The best soccer ball ever!',
                 image: "imgs/soccer/ball.png",
+                category: 'soccer',
             },
             {
+                id: 2,
                 name: 'Tubing raft',
                 price: 624.00,
                 description: 'Have some extreme fun being pulled in this raft!',
                 image: "imgs/watersports/tube.png",
+                category: 'watersports',
             },
             {
+                id: 3,
                 name: 'Chess board and pieces',
                 price: 499.00,
-                description: 'finest wood',
+                description: 'Finest wood',
                 image: "imgs/Boardgames/chaseSet.png",
+                category: 'boardgames',
             },
             {
+                id: 4,
                 name: 'Team Canada soccer jersey',
                 price: 591.00,
                 description: 'Display your national pride with this authentic Team Canada soccer jersey!',
                 image: "imgs/soccer/shirtCanada.png",
+                category: 'soccer',
             },
             {
+                id: 5,
                 name: 'Goggles',
                 price: 396.00,
                 description: 'Top-of-the-line goggles for the most extreme of watersports!',
                 image: "imgs/watersports/sunglass.png",
+                category: 'watersports',
             },
         ]
 };
+
+export function getItemFields () {
+    return [
+        'name',
+        'price',
+        'category',
+    ]
+}
