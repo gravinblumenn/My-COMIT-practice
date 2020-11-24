098e6c1b37601b0233bc998c37b1e66c693adfc8
gravinblumenn <gavinblumenn@gmail.com>
2020-11-24 16:31:26 -0600

    changed firebase db for notes app

--- notes-app-react/src/firebase/index.js
+++ notes-app-react/src/firebase/index.js
@@ -1,15 +1,17 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDeLLumatMD_w0pXhBapTmyN41dDGK1bJM",
-    authDomain: "general-store-project.firebaseapp.com",
-    databaseURL: "https://general-store-project.firebaseio.com",
-    projectId: "general-store-project",
-    storageBucket: "general-store-project.appspot.com",
-    messagingSenderId: "125355591954",
-    appId: "1:125355591954:web:7e3bebcf60fe7270f68bcb"
-  };
-
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyD8E9o7oLghfoWDA8ZMK-DxXsLE7TAzyCM",
+  authDomain: "comit-notes-react-app.firebaseapp.com",
+  databaseURL: "https://comit-notes-react-app.firebaseio.com",
+  projectId: "comit-notes-react-app",
+  storageBucket: "comit-notes-react-app.appspot.com",
+  messagingSenderId: "663546755822",
+  appId: "1:663546755822:web:42af588dbf57fdcd8411ff",
+  measurementId: "G-LXQGQ3M8M5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
   export db = app.database();
   export auth = app.auth();
